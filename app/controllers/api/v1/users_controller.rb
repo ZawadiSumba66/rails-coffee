e7970c9068275697c6eda0403a68e5c0b70c29cd
ZawadiSumba66 <rosesumba98@gmail.com>
2022-02-15 16:12:19 +0300

    Edit user method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,9 +12,22 @@ class Api::V1::UsersController < ApiController
         render json: user
     end
 
+    def edit
+        user = User.find(params[:id])
+    end
+
+    def update
+        user = User.find(params[:id])
+        if user.update(params)
+            render json: user, status: 200
+        else
+            render json: {message: 'An error occurred while updating your profile'}, status: 400
+        end
+    end
+
     private
 
     def user_params
       params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
     end 
-end
+end 
