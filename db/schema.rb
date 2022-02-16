8e98ade33211ccbd52c2c6c5b09e5dd1e669def4
ZawadiSumba66 <rosesumba98@gmail.com>
2022-02-16 15:43:35 +0300

    Migrate coffee to schema db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_15_102016) do
+ActiveRecord::Schema.define(version: 2022_02_16_090220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,18 @@ ActiveRecord::Schema.define(version: 2022_02_15_102016) do
     t.index ["user_id"], name: "index_avatars_on_user_id"
   end
 
+  create_table "coffees", force: :cascade do |t|
+    t.string "size"
+    t.string "milk"
+    t.string "topping"
+    t.decimal "quantity"
+    t.decimal "price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_coffees_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -84,4 +96,5 @@ ActiveRecord::Schema.define(version: 2022_02_15_102016) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "avatars", "users"
+  add_foreign_key "coffees", "users"
 end
