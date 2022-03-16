7ba4c77fe73cb854aa28ab8684a0235cd987a08b
ZawadiSumba66 <rosesumba98@gmail.com>
2022-03-16 21:23:42 +0300

    Creat coffee posts

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::PostsController < ApiController
+    skip_before_action :authenticate_user!, only: %i[index show]
+    def index
+       popular = Category.first.posts.all
+       latte = Category.second.posts.all
+       espresso = Category.third.posts.all
+       render json: {popular: popular, latte: latte, espresso: espresso}, status: 200
+    end
+    def create
+        if current_user.admin?
+            post = current_user.posts.create(post_params)
+            if post.save
+            render json: post,  status: 200
+            else
+            render json: { message: post.errors.full_messages }, status: 400
+            end
+        end
+    end
+
+    def show
+        post = Post.find(params[:id])
+        render json: post
+    end
+    
+    private
+    def post_params
+        params.require(:post).permit(:name, :image, :category_id);
+    end
+end
