40108e7b44bb9b85da12612ff02169ae69cc6124
ZawadiSumba66 <rosesumba98@gmail.com>
2022-03-16 21:28:23 +0300

    Add routes for posts and categories

--- config/routes.rb
+++ config/routes.rb
@@ -8,18 +8,17 @@ Rails.application.routes.draw do
     namespace :v1 do
       post :auth, to: 'authentication#create'
       post '/presigned_url', to: 'direct_upload#create'
+      get '/profile_photo', to: 'avatars#user_avatar'
       devise_for 'User', at: 'auth', skip: [:omniauth_callbacks]
       post 'social_auth/callback', to: 'social_auth_controller#authenticate_social_auth_user' # this is the line where we add our routes
       resources :users
       resources :avatars
       resources :coffees
-      resources :espressos, only: %i[index show]
-      resources :lattes, only: %i[index show]
-      resources :populars, only: %i[index show]
+      resources :posts, only: %i[index show]
+      resources :categories, only: %i[index show]
       scope '/admin' do
-        resources :espressos, only: :create
-        resources :lattes, only: :create
-        resources :populars, only: :create
+        resources :posts, only: :create
+        resources :categories, only: :create
       end
     end
   end
